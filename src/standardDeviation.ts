f3d2683fdefff2a8530bad47c2c8132991a38fc0
Bryan Lenihan <bryan@bryan-lenovo-desktop.rolesvillesoftware.com>
2018-03-18 20:34:32 -0400

    Added Standard Deviation

---
+++ src/standardDeviation.ts
@@ -0,0 +1,19 @@
+import { population } from "./population";
+import { mean } from "./mean";
+
+export function min<T>(items: T[], field: string = null): number {
+    let pop = population(items, field);
+    if (pop == null || pop.length <= 1) {
+        return 0;
+    }
+
+    const m = mean(pop);
+    let s = 0;
+
+    pop.forEach(item => {
+        s += (item - m) ^ 2;
+    });
+
+    let d = s / (pop.length - 1);
+    return Math.sqrt(d);
+}
